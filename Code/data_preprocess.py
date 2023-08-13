011b5736da345868f36049642229881c7c1b6468
MLLSCKR <98379223+MLLSCKR@users.noreply.github.com>
2023-08-13 15:53:21 +0900

    add training tokenizer code and preprocess code

---
+++ Code/data_preprocess.py
@@ -0,0 +1,56 @@
+from transformers import AdamW
+from transformers import get_scheduler
+from transformers import DataCollatorForSeq2Seq
+from datasets import load_dataset
+from transformers import AutoTokenizer
+
+from tokenizers import Tokenizer
+from tokenizers.models import BPE
+
+def preprocess(examples):
+    # tok_inputs = tokenizer.encode(inputs).ids
+    # tok_targets = tokenizer.encode(targets).ids
+    tok_inputs = []
+    tok_targets = []
+
+    sos_id = tokenizer.token_to_id('[SOS]')
+    eos_id = tokenizer.token_to_id('[EOS]')
+
+    for example in examples['translation']:
+        tok_input = tokenizer.encode(example['en']).ids
+        tok_input.insert(0, sos_id) # ADD SOS TOKEN
+        tok_input.append(eos_id) # ADD EOS TOKEN
+        tok_inputs.append(tok_input)
+
+    for exmaple in examples['translation']:
+        tok_target = tokenizer.encode(example['de']).ids
+        tok_target.insert(0, sos_id) # ADD SOS TOKEN
+        tok_target.append(eos_id) # ADD EOS TOKEN
+        tok_targets.append(tok_target)
+
+    model_inputs = {'input_ids':tok_inputs, 'labels':tok_targets}
+
+    return model_inputs
+
+if __name__ == "__main__":
+    # load  datasets
+    raw_datasets = load_dataset('wmt16', 'de-en')
+
+    # load tokenizer
+    file_dict = {
+        'tokenizer_30000': './WMT_16_TOKENIZER/wmt_16_30000.json',
+        'tokenizer_20000': './WMT_16_TOKENIZER/wmt_16_20000.json',
+        'tokenizer_10000': './WMT_16_TOKENIZER/wmt_16_10000.json',
+    }
+
+    with open(file_dict['tokenizer_20000'], 'r') as f:
+        tokenizer = Tokenizer.from_str(''.join(f.readlines()))
+
+    # preprocess datasets
+    tokenized_ds = raw_datasets.map(
+        preprocess,
+        batched = True
+    )
+
+    # save preprocessed dataset
+    tokenized_ds.save_to_disk('./WMT_16_PREPROCESSED')
